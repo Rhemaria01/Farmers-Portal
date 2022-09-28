63f3564467cf30b9e25dc16b4d90e36ac554b4bb
reenav01 <reenavh@sjcem.edu.in>
2022-09-28 23:19:25 +0530

    Util added

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,11 @@
 export const FirebaseConfig = {
-    //enter your firebase config here
+    apiKey: "AIzaSyAndkaetkM_5BPbxAiARnHyG__piv_NsFI",
+    authDomain: "farmerportal-b46d1.firebaseapp.com",
+    databaseURL: "https://farmerportal-b46d1-default-rtdb.firebaseio.com",
+    projectId: "farmerportal-b46d1",
+    storageBucket: "farmerportal-b46d1.appspot.com",
+    messagingSenderId: "594247722407",
+    appId: "1:594247722407:web:eccaf4dc376b6bdd2cc282"
 }
 export const imageConfig = {
     quality: 0.2,
